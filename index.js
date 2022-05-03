0c7fd2e287a5d00783de61d6ab06476dacfa4346
ABU-ISA-BISWAS <bmabuisa.cse@gmail.com>
2022-05-04 04:12:24 +0600

    create fruits data

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.duuuz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run(){
+    try{
+        await client.connect();
+        const fruitCollection = client.db('fruit-warehouse').collection('fruit');
+
+        app.get('/fruit',async(req,res) => {
+            const query = {};
+            const cursor =fruitCollection.find(query);
+            const fruits = await cursor.toArray();
+            res.send(fruits);
+        })
+
+
+    }
+    finally{
+        
+    }
+}
+run().catch(console.dir);
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello fruit warehouse')
+});
+
+app.listen(port, () => {
+  console.log(`fruit warehouse running on port ${port}`)
+});
+
