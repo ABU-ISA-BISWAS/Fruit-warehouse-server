f1396b4818fa9b12df02c7f784cb4b3b2143d607
ABU-ISA-BISWAS <bmabuisa.cse@gmail.com>
2022-05-05 00:30:45 +0600

    delete data

--- index.js
+++ index.js
@@ -44,6 +44,15 @@ async function run(){
         res.send(result);
      });
 
+     app.delete('/fruit/:id',async(req,res)=>{
+      const id =req.params.id;
+      console.log(id);
+      const query = {_id: ObjectId(id)};
+      console.log(query);
+      const result = await fruitCollection.deleteOne(query);
+      res.send(result);
+   });
+
 
     }
     finally{
